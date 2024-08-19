2e7a64ffcde3efdeabbf1ee67c82189840e92b6e
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-19 17:14:16 +0100

    Update 0-simple_helper_function.py

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,7 @@
 
 """ A function that takes in two argument and return a turple
 """
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
   page = (page - 1) * page_size
   page_size = page + page_size
   return (page, page_size)
