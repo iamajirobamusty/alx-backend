a23b2a09cf2e80ccfb5923e4ceb86b13b7d75f43
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-27 21:01:39 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ def home():
     Return: return_description
     """
     
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port="5000",host='0.0.0.0')
