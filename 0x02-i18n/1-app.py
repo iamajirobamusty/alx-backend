61eca9f6d196ae85185fef5cb990c9a0ab2bfe7b
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-27 21:02:38 +0100

    Create 1-app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    """Class to set the configuration for the app.
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LANGUAGE = 'en'
+    BABEL_DEFAULT_TIMEZONE ='UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    """Home page
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    return render_template('1-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port="5000",host='0.0.0.0')
