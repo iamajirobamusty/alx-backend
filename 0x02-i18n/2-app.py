98c31889aa74520a5bf696ef16abd13c857641e4
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-27 21:10:52 +0100

    Create 2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+class Config:
+    """Class to set the configuration for the app.
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LANGUAGE = 'en'
+    BABEL_DEFAULT_TIMEZONE ='UTC'
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+@babel.localeselector
+def get_locale():
+    """Funciton to find the best mtach for the supported languages.
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    return request.accetpt_languages.best_match(app.config["LANGUAGES"])
+  
+
+@app.route('/')
+def home():
+    """Home page
+    
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,port="5000",host='0.0.0.0')
