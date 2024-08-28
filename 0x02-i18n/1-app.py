8d2b33b91ae92feda1c943e8797585dd200d408c
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-28 10:35:28 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -19,7 +19,7 @@ class Config:
     Return: return_description
     """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LANGUAGE = 'en'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
