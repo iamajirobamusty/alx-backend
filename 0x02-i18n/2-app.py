38954ed271ade32b0c4fdb28820c59d97162cd8d
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-28 10:45:57 +0100

    Update 2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+""" 2-app.py
+    A simple flask ask implementing i18n and i10n
+"""
+
+
 from flask import Flask, render_template
 from flask_babel import Babel
 
+
 app = Flask(__name__)
+
 
 class Config:
     """Class to set the configuration for the app.
@@ -11,17 +18,18 @@ class Config:
     argument -- description
     Return: return_description
     """
-    
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LANGUAGE = 'en'
-    BABEL_DEFAULT_TIMEZONE ='UTC'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    
 
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @babel.localeselector
 def get_locale():
-    """Funciton to find the best mtach for the supported languages.
+    """Function find the best match for the supported languages.
     
     Keyword arguments:
     argument -- description
@@ -43,4 +51,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,port="5000",host='0.0.0.0')
+    app.run(debug=True, port="5000", host='0.0.0.0')
