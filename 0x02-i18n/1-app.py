64a26d148fbea27d6774f2e6b23ad24658e27530
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-28 11:04:31 +0100

    Update 1-app.py

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python3
+"""A simple flask app
+"""
 
-"""
-1-main.py
-A simple Flask app representing internationalization and localization.
-"""
 
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
 
 class Config(object):
-    """Class to set the configuration for the app."""
+    """_summary_
 
+    Returns:
+            _type_: _description_
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
+# configure the flask app
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
+
 @app.route('/')
-def home():
-    """Home page."""
+def index():
+    """_summary_
+    """
     return render_template('1-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(port="5000", host="0.0.0.0", debug=True)
