d933bb654b8a7aa40c2d114e06a0eabbf2d8501c
Ajiroba Mustapha Abiodun <110599927+iamajirobamusty@users.noreply.github.com>
2024-08-28 10:10:48 +0100

    Update 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template
 from flask_babel import Babel
 """ 0-app.py
 
+
 app = Flask(__name__)
 babel = Babel(app)
+
 
 @app.route('/')
 def home():
@@ -17,5 +19,6 @@ def home():
     
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
-    app.run(debug=True,port="5000",host='0.0.0.0')
+    app.run(debug=True, port="5000", host='0.0.0.0')
